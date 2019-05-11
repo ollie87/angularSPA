4e0975602e7e5b75e640d9b16b7b18c576da76d9
Mario <mariogv87@gmail.com>
2019-05-11 09:09:43 +0200

    routes & resources

---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
+
+
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styles: []
+})
+export class HeroeComponent implements OnInit {
+
+  heroe: any = {};
+
+  constructor(private activateRoute: ActivatedRoute, private heroesService: HeroesService) {
+    this.activateRoute.params.subscribe( params => {
+      this.heroe = this.heroesService.getHeroe(params.id);
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, IHeroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styles: []
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: IHeroe[] = [];
+
+  constructor( private heroesService: HeroesService, private router: Router) { }
+
+  ngOnInit() {
+     this.heroes = this.heroesService.getHeroes();
+  }
+
+  viewHero(idx: number) {
+    this.router.navigate(['/heroe', idx]);
+  }
+}
